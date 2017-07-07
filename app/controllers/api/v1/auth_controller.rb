5c7e597e47098e4c9eaf2c73cf1d5ea898044dff
iwpeifer <iwpeifer@gmail.com>
2017-07-07 02:00:13 -0400

    fixed auth bug that forced users to log in after every refresh

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,19 +3,19 @@ class Api::V1::AuthController < ApplicationController
   before_action :authorize_user!, only: [:show]
 
   def show
-    id = request.headers['Authorization']
-    user = User.find_by(id: id)
-    if user.present?
+    # id = request.headers['Authorization']
+    # user = User.find_by(id: id)
+    # if user.present?
       render json: {
-        id: user.id,
-        username: user.username,
-        friends: user.send_friends
+        id: current_user.id,
+        username: current_user.username,
+        friends: current_user.send_friends
       }
-    else
-      render json: {
-        error: 'No user id present on the headers'
-      }, status: 404
-    end
+    # else
+    #   render json: {
+    #     error: 'No user id present on the headers'
+    #   }, status: 404
+    # end
   end
 
   def create
@@ -25,7 +25,7 @@ class Api::V1::AuthController < ApplicationController
         id: user.id,
         username: user.username,
         friends: user.send_friends,
-        jwt: JWT.encode({user_id: 1}, ENV['JWT_SECRET'], ENV['JEW_ALGORITHM'])
+        jwt: JWT.encode({user_id: 1}, ENV['JWT_SECRET'], ENV['JWT_ALGORITHM'])
       }
     else
       render json: {
